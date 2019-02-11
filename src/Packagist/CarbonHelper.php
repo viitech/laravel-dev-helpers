55e5e67623892414cb82ebf48c4560eac7177f73
Ahmed Yusuf <ahmed.yusuf@viitech.net>
2019-02-11 16:32:23 +0300

    Carbon Helper changes

--- src/Packagist/CarbonHelper.php
+++ src/Packagist/CarbonHelper.php
@@ -6,23 +6,35 @@ use Carbon\Carbon;
 use Exception;
 use MongoDB\BSON\UTCDateTime;
 use VIITech\Helpers\Constants\Attributes;
+use VIITech\Helpers\Constants\EnvVariables;
 
 class CarbonHelper
 {
 
     /**
+     * Generate Now Timestamp
+     * @param string $format
+     * @return string
+     */
+    static function generateNowTimestamp($format = 'c')
+    {
+        return Carbon::now(env(EnvVariables::APP_TIMEZONE))->format($format);
+    }
+
+    /**
      * Get Formatted Carbon Date From UTC DateTime
      * @param UTCDateTime $value
+     * @param string $format
      * @return mixed
      */
-    function getFormattedCarbonDateFromUTCDateTime($value)
+    static function getFormattedCarbonDateFromUTCDateTime($value, $format = 'c')
     {
         try {
-            if(!is_null($value)){
+            if (!is_null($value)) {
                 if (is_array($value) && isset($value[Attributes::DATE])) {
                     return $value[Attributes::DATE];
                 }
-                return Carbon::instance($value->toDateTime())->format('c');
+                return Carbon::instance($value->toDateTime())->format($format);
             }
             return $value;
         } catch (Exception $e) {
